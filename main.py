fb270ee9b7c8e60a787fde9c917778f544190e57
hris <81253537+hris69@users.noreply.github.com>
2021-07-09 16:38:49 +0200

    Update main.py
    Main file for your Python Discord bot

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-# main file
+""""
+Copyright © Hris 2021 - https://github.com/hris69
+Version: 1.0
+"""
+import discord
+from dotenv import load_dotenv
+load_dotenv()
+
+intents = discord.Intents.all() # enables all intents this requires the two switches ticket at https://discord.com/developers
+client = commands.Bot(command_prefix="your_prefix_here", intents=intents)
+client.remove_command('help') # remove the default help command if you want a custom one
+
+@client.event
+async def on_ready():
+    print(f"{client.user} has been started!") # if you want you can customize this
+    
+    
+# for filename in os.listdir('./cogs'): - if you have cogs you can use this to load all your cogs in the cogs directory
+   # if filename.endswith('.py'):
+    #    client.load_extension(f'cogs.{filename[:-3]}') 
+
+    
+token = os.getenv("TOKEN") # gets the token for the .env file, secure your token with the dotenv library
+client.run(token) # runs the bot with the given token
